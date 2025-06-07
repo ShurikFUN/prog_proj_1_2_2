8f6c915e28cc597fe7b021cfcaf05230aba87984
ShurikFUN <paulprojectsworking@gmail.com>
2025-06-07 01:06:28 -0700

    Update admin.py

--- routers/handlers/admin.py
+++ routers/handlers/admin.py
@@ -2,8 +2,6 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-import asyncio
-from aiogram import Bot
 
 router = Router()
 
