e0cc63d2d39577c64388fc0dcc8a2ebd0255ff9d
ShurikFUN <paulprojectsworking@gmail.com>
2025-06-08 00:52:45 -0700

    Update command.py

--- filters/command.py
+++ filters/command.py
@@ -2,5 +2,5 @@ from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 class NotCommandFilter(BaseFilter):
-    async def __call__(self, message: Message) -> bool:
+    async def __call__(self, message: Message):
         return message.text is not None and not message.text.startswith('/')
